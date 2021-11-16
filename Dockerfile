bb1553f6f3ec1b547d8a3bd1d5e18183a5f6ca42
Tim Seagren <tim.seagren.ctr@us.af.mil>
2021-11-15 16:50:48 -0800

    adding Makefile and cleaning up setup.sh

--- Dockerfile
+++ Dockerfile
@@ -1,13 +1,11 @@
 FROM golang:1.17 as build
 WORKDIR /app
-COPY go.mod go.sum .
-
-RUN go mod download
 COPY . .
-RUN CGO_ENABLED=0 GOOS=linux go build -a -ldflags '-extldflags "-static"' -o imageswap main.go
+RUN go mod download && \
+  CGO_ENABLED=0 GOOS=linux go build -a -ldflags '-extldflags "-static"' -o imageswap main.go
 
 FROM gcr.io/distroless/base
 COPY --from=build /app/imageswap /
-EXPOSE 8443 8080
+EXPOSE 8443
 
 CMD ["/imageswap"]
--- internal/deploy/setup.sh
+++ internal/deploy/setup.sh
@@ -1,16 +1,14 @@
 #!/bin/bash
+deploy_dir="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
 # Start KinD and deploy local registry
-./kind-with-registry.sh
+$deploy_dir/kind-with-registry.sh
 
 # Deploy webhook resources
-kustomize build . | kubectl apply -f -
+kustomize build $deploy_dir | kubectl apply -f -
 
 # Create TLS keypair and CA bundle for webhook to use
-./webhook-create-signed-cert.sh
+$deploy_dir/webhook-create-signed-cert.sh
 
 # Create mutating webhook with CA
-cat mutatingwebhook.yaml | ./webhook-patch-ca-bundle.sh | kubectl apply -f -
-
-# Create test pod to see if the MWH works
-kubectl run alpine --image=alpine --restart=Never -n default --command -- sleep infinity
+cat $deploy_dir/mutatingwebhook.yaml | $deploy_dir/webhook-patch-ca-bundle.sh | kubectl apply -f -
